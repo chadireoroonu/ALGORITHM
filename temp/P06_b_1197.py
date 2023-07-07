4a28326d0d53b7063eb7cf005074b181353f5b86
chadireoroonu <chadireoroonu@gmail.com>
2023-07-07 17:41:33 +0900

    temp

--- temp/P06_b_1197.py
+++ temp/P06_b_1197.py
@@ -1,15 +1,37 @@
+# 연결 정보 담고 정렬
+# 앞에서부터 하나씩 뽑기
+# 부모 노드 검사
+# 부모 노드가 다르다면 두 노드 연결
+# 연결 시 가중치 더하기
+
 import sys
-from queue import PriorityQueue
-from collections import deque
 sys.stdin = open('input.txt')
 
 V, E = map(int, sys.stdin.readline().split())
-node = [[]for _ in range(V+1)]
+parent = [0] * (V+1)
+queue = []
+result = 0
 
-for _ in range(E):
-    a, b, p = map(int, sys.stdin.readline().split())
-    node[a].append([p, b])
-    node[b].append([p, a])
+for i in range(E):
+    a, b, c = map(int, sys.stdin.readline().split())
+    queue.append([a, b, c])
 
-que = PriorityQueue()
-used = deque()
+queue.sort()
+
+
+def check(n):
+    if parent[n] and :
+
+    return
+
+
+for i in range(len(queue)):
+    x = queue[i][1]
+    y = queue[i][2]
+    if x < y:
+        x, y = y, x
+    if check(x) and check(y) and check(x) != check(y):
+        parent[y] = x
+        result += queue[i][0]
+
+print(result)
