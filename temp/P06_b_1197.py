3f8f1dad9988707d13985a8187d7e0805edf8e28
chadireoroonu <chadireoroonu@gmail.com>
2023-07-07 21:18:23 +0900

    file

--- temp/P06_b_1197.py
+++ temp/P06_b_1197.py
@@ -1,15 +1,35 @@
 import sys
-from queue import PriorityQueue
-from collections import deque
 sys.stdin = open('input.txt')
 
+def check(num):
+    if parent[num] != num:
+        parent[num] = check(parent[num])
+    return parent[num]
+
 V, E = map(int, sys.stdin.readline().split())
-node = [[]for _ in range(V+1)]
+parent = [0 for _ in range(V+1)]
+node = []
+result = 0
 
 for _ in range(E):
     a, b, p = map(int, sys.stdin.readline().split())
-    node[a].append([p, b])
-    node[b].append([p, a])
+    node.append([p, a, b])
 
-que = PriorityQueue()
-used = deque()
+node.sort()
+
+for z in range(E):
+    x = check(node[z][1])
+    y = check(node[z][2])
+    print(f'node={node}, z={z}')
+    print(f'x={x}, y={y}')
+    print(f'parent={parent}')
+    if x < y:
+        x, y = y, x
+    if x and y and x != y:
+        parent[x] = y
+        result += node[z][0]
+    elif not x and not y:
+        parent[x] = y
+        result += node[z][0]
+
+print(result)
