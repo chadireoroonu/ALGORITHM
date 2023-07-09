0d43c63babf9cfa8b64fe4fb4bb8688bd7b28e22
chadireoroonu <chadireoroonu@gmail.com>
2023-07-09 21:32:12 +0900

    add

--- 23_07/J09_b_11866.java
+++ 23_07/J09_b_11866.java
@@ -11,18 +11,20 @@ public class J09_b_11866 {
 		
 		LinkedList<Integer> queue = new LinkedList<>();
 		
+		// queue 요소 추가
 		for (int i = 1; i <= N; i++) {
 			queue.add(i);
 		}
 		
 		System.out.print("<");
+		// K 번째 수 출력 N-1회 반복
 		for (int j = 0; j < N-1; j++) {
 			for (int k = 0; k < K-1; k++) {
 				queue.addLast(queue.pollFirst());
 			}
 			System.out.print(queue.poll()+", ");
 		}
-			
+		// 마지막 요소 출력
 		System.out.println(queue.poll()+">");
 	}
 }
