66c85f6dea594be5b3738be63caf8a42a04b8b9c
chadireoroonu <chadireoroonu@gmail.com>
2023-07-16 21:26:58 +0900

    temp

--- temp/P13_b_9466.py
+++ temp/P13_b_9466.py
@@ -5,14 +5,12 @@ sys.stdin = open('input.txt')
 def cycle(n):
     temp = 1
     queue = deque()
-    queue.append(students[n])
-    visited = [0]*N
-    visited[students[n]-1] = 1
+    queue.append(students[n-1])
     while queue:
         j = queue.popleft()
-        nj = students[j-1]
-        if nj == n:
-            return temp + 1
+        nj = students[j - 1]
+        if nj == n-1 and not visited[n-1]:
+            return temp
         if not visited[nj]:
             visited[nj] = 1
             queue.append(nj)
@@ -25,11 +23,14 @@ for tc in range(T):
     N = int(sys.stdin.readline())
     students = list(map(int, sys.stdin.readline().split()))
     num = 0
+    visited = [0]*(N+1)
 
     for i in range(N):
         if students[i] == i + 1:
+            visited[students[i] - 1] = 1
             num += 1
-        elif students[i]:
+        elif not visited[i]:
+            visited[students[i] - 1] = 1
             num += cycle(i)
 
     print(N-num)
