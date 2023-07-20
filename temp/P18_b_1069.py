9bf83d8c95a87202bcd8f38a9356b830709176c6
chadireoroonu <chadireoroonu@gmail.com>
2023-07-20 17:50:41 +0900

    temp

--- temp/P18_b_1069.py
+++ temp/P18_b_1069.py
@@ -1,9 +1,8 @@
-# 2차원 배열 visited 만들기
-# BFS 이동
-# 이동 시, 대각선 방향의 이동 가능 여부 생각하기
+# 원형으로 이동 가능 경로 구하기
 # 0, 0 도착 시 시간 출력
 
 import sys
 sys.stdin = open('input.txt')
 
 X, Y, D, T = map(int, sys.stdin.readline().split())
+
