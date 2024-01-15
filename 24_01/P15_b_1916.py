fc9ae9cae65b3aaad95730c26643dcd556d1a480
chadireoroonu <chadireoroonu@gmail.com>
2024-01-15 23:53:44 +0900

    python algorithm

---
+++ 24_01/P15_b_1916.py
@@ -0,0 +1,37 @@
+import sys, heapq
+sys.stdin = open("../temp/input.txt")
+
+def dijkstra(num):
+    visited = [100001 * N for _ in range(N+1)]
+    visited[num] = 0
+    queue = []
+    heapq.heappush(queue, [visited[num], num])
+
+    while queue:
+        count, now = heapq.heappop(queue)
+
+        if count > visited[now]:
+            continue
+
+        for i in load[now]:
+            if visited[i[0]] > count + i[1]:
+                heapq.heappush(queue, [count + i[1], i[0]])
+                visited[i[0]] = count + i[1]
+
+    return visited
+
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+
+load = [[] for _ in range(N+1)]
+
+for i in range(M):
+    S, E, M = list(map(int, sys.stdin.readline().split()))
+    load[S].append([E, M])
+
+start, end = list(map(int, sys.stdin.readline().split()))
+
+result = dijkstra(start)
+
+print(result[end])
