f7b30a071e7c7608ff298d5e7837a99468f45e86
chadireoroonu <chadireoroonu@gmail.com>
2024-03-19 23:50:41 +0900

    edit

--- 24_03/P19_b_13904.py
+++ 24_03/P19_b_13904.py
@@ -3,7 +3,7 @@ sys.stdin = open("../temp/input.txt")
 
 N = int(sys.stdin.readline())
 queue = []  # 마감일 우선 과제 정보 정렬
-dp = []  # 점수 낮은순 정렬
+total = []  # 점수 낮은순 정렬
 
 for _ in range(N):
     limit, score = map(int, sys.stdin.readline().split())
@@ -12,12 +12,12 @@ for _ in range(N):
 while queue:
     limit, score = heapq.heappop(queue)
     # 마감일 내에 할 수 있는 일이라면 추가
-    if len(dp) < limit:
-        heapq.heappush(dp, score)
+    if len(total) < limit:
+        heapq.heappush(total, score)
     # 마감일까지 과제 일정이 차있는 경우, 제일 낮은 점수와 비교 후 교체
     else:
-        if dp[0] < score:
-            heapq.heappop(dp)
-            heapq.heappush(dp, score)
+        if total[0] < score:
+            heapq.heappop(total)
+            heapq.heappush(total, score)
 
-print(sum(dp))
+print(sum(total))
