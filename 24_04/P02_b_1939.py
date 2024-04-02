ce0b63d8a66c814ef0937c1b51132d2ce0e5d63f
chadireoroonu <chadireoroonu@gmail.com>
2024-04-02 23:53:04 +0900

    2404022 python algorithm

---
+++ 24_04/P02_b_1939.py
@@ -0,0 +1,35 @@
+import sys, heapq
+sys.stdin = open("../temp/input.txt")
+
+N, M = map(int, sys.stdin.readline().split())
+loads = [[] for _ in range(N + 1)]  # 다리 연결 및 중량제한
+
+for _ in range(M):  # 최대힙으로 사용하기 위해 -C 형태로 저장
+    A, B, C = map(int, sys.stdin.readline().split())
+    loads[A].append([-C, B])
+    loads[B].append([-C, A])
+
+S, E = map(int, sys.stdin.readline().split())
+maxi = [0] * (N + 1)  # 출발지에서 각 섬까지의 최대 중량
+
+queue = []
+heapq.heappush(queue, [0, S])
+maxi[S] = 0
+while queue:
+    cost, x = heapq.heappop(queue)
+    # 목적지에 도착했다면 중단
+    if x == E:
+        break
+    # 현재 무게보다 더 큰 중량제한이 저장되어 있다면 무시
+    if maxi[x] < cost:
+        continue
+    # 연결된 섬까지 중량제한, 섬 번호 저장
+    for temp, nx in loads[x]:
+        # cost가 0이 아니면, cost, temp 중 작은 무게로 할당
+        select = max(cost, temp) if cost else temp
+        # 저장된 무게보다 큰 중량제한일 경우 maxi 재할당
+        if maxi[nx] > select:
+            maxi[nx] = select
+            heapq.heappush(queue, [select, nx])
+
+print(-maxi[E])
